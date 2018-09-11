e7a522578f71481cdf3f76e694be359285757ba7
rohmattaufik <rohmat.taufik14@gmail.com>
2018-09-11 14:27:04 +0700

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('username');
             $table->string('email');
-            $table->string('name');
+            $table->string('nama');
             $table->integer('role');
             $table->integer('wisata_id')->nullable();
             $table->string('password');
